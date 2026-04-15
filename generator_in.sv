67c502883e03eb4c1af80dbd3b5ec0c162123cfc
alinavadana <123012409+alinavadana@users.noreply.github.com>
2026-04-15 12:48:37 +0300

    Add files via upload

---
+++ generator_in.sv
@@ -0,0 +1,50 @@
+class generator_in;
+
+rand transaction_in trans, tr;
+
+int trans_cnt;
+
+mailbox gen2driv;
+
+event ended;
+
+function new(mailbox gen2driv, event ended);
+
+    this.gen2driv = gen2driv;
+    this.ended    =ended;
+    trans = new();
+
+endfunction
+
+task main();
+
+ repeat(trans_cnt) begin
+    if (!trans.randomize())
+        $error("Generator failed to randomize");
+    tr = trans.do_copy();
+    gen2driv.put(tr);
+ end
+ -> ended;
+endtask
+
+task write_register(bit [1:0] addr, bit [7:0] data );
+
+    if (!trans.randomize() with {d_in ==data; addr_i==addr; valid_i==1; rd_wr_i==0;})
+        $error("Generator failed to randomize");
+    tr = trans.do_copy();
+    gen2driv.put(tr);
+
+endtask
+
+task read_register(bit [1:0] addr );
+
+    if (!trans.randomize() with { addr_i==addr; valid_i==1; rd_wr_i==1;})
+        $error("Generator failed to randomize");
+    tr = trans.do_copy();
+    gen2driv.put(tr);
+
+endtask
+
+
+
+endclass
