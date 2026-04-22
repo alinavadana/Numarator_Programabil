f1dbb50235a23e800b4eb05b74d6b840a04d2629
alinavadana <123012409+alinavadana@users.noreply.github.com>
2026-04-22 13:26:46 +0300

    Create test_simplu.sv

---
+++ test_simplu.sv
@@ -0,0 +1,23 @@
+
+`include "environment.sv"
+
+program test(interface_in in_intf, interface_out out_intf);
+
+    environment env;
+
+    initial begin
+    $error("%0t a inceput testul", $time);
+        // Instanțiem mediul trecând ambele interfețe către constructorul clasei environment 
+        env = new(in_intf, out_intf);
+        
+        // Setăm numărul de tranzacții ce urmează a fi generate de generator_in 
+        env.gen.trans_cnt = 0;
+
+        env.gen.write_register(0, 8'b11001000);
+        env.gen.read_register(0);
+        
+        // Pornim execuția fluxului de test 
+        env.run();
+    end
+
+endprogram
