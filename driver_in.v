43658c93b02c55cc05977ba57acdab9bb21c66c6
alinavadana <123012409+alinavadana@users.noreply.github.com>
2026-04-22 12:36:10 +0300

    Update driver_in.v

--- driver_in.v
+++ driver_in.v
@@ -1,83 +1,76 @@
 class driver_in;
 
- int no_transactions;
- 
- virtual interface_in in_vif;
- mailbox gen2driv;
- 
-funtion new( virtual interface_in in_vif, mailbox gen2driv);
+  int no_transactions;
 
- this.in_vif=in_vif;
- this.gen2driv=gen2driv;
- 
-endfunction
+  virtual interface_in in_vif;
+  mailbox gen2driv;
 
-task reset;
-//Asteptam front negativ de reset -> activare reset
- @(negedge in_vif.rst_ni);
- 
- $diaplay("[%t0]----[DRIVER] RESET STARTED----:", $time)
-  in_vif.driver_cb.valid_i <= 1'b0;
-  in_vif.driver_cb.rd_wr_i <=1'b0;
-  in_vif.driver_cb.addr_i  <=2'b0;
-  in_vif.driver_cb.d_in    <=8'b0;
+  function new(virtual interface_in in_vif, mailbox gen2driv);
+    this.in_vif   = in_vif;
+    this.gen2driv = gen2driv;
+  endfunction
 
-endtask;
+  task reset;
+    // Asteptam front negativ de reset -> activare reset
+    @(negedge in_vif.rst_ni);
 
-task drive;
+    $display("[%0t] ---- [DRIVER] RESET STARTED ----", $time);
+    in_vif.driver_cb.valid_i <= 1'b0;
+    in_vif.driver_cb.rd_wr <= 1'b0;
+    in_vif.driver_cb.addr_i  <= 2'b0;
+    in_vif.driver_cb.d_in    <= 8'b0;
+  endtask
 
-   transaction trans;
-   
-   gen2driv.get(trans);
-   $diaplay("----[DRIVER STARTED : %0d] ---:", no_transactions);
-   
-   
-   //asteptam delay-ul
-   repeat (trans.delay)
-     @(posedge in_vif.clk);
-	 //incepe tranzactia ->valid 1
-	 in_vif.driver_cb.valid_i <=1'b0;
-	 
-	 if (trans.rd_wr==0) begin
-	    in_vif.driver_cb.rd_wr_i<=1'b0;
-		
-	 else begin
-	    in_vif.driver_cb.rd_wr_i<=1'b1;
-	 end
-	 
-	 //daca e scriere ->punem date pe d_in
-	 if(trans.rd_wr_i ==0 ) begin
-	   in_vif.driver_cb.d.in <= trans.d_in;
-	 end
-	 
-	 @(posedge in_vif.clk);
-	 in_vif.driver_cb.valid_i <=1'b0;
-	 
-	 no_transactions++;
-	 
-endtask
+  task drive;
+    transaction_in trans;
 
-task main;
+    gen2driv.get(trans);
+    $display("---- [DRIVER STARTED : %0d] ----", no_transactions);
 
-forever begin
-   
-  while ( in_vif.rst_ni == 0)
-  @posedge in_vif.clk;
-   
-  fork
-    begin
-	  drive();
-	end
-	
-	begin
-	  reset();
-	end
-	
-  join_any;
-  disable fork;
-  
-end
+    // asteptam delay-ul
+    repeat (trans.delay)
+      @(posedge in_vif.clk);
 
-endtask
+    // incepe tranzactia
+    in_vif.driver_cb.valid_i <= 1'b1;
+    in_vif.driver_cb.addr_i  <= trans.addr_i;
 
+    if (trans.rd_wr == 0) begin
+      in_vif.driver_cb.rd_wr <= 1'b0;
+    end
+    else begin
+      in_vif.driver_cb.rd_wr <= 1'b1;
+    end
 
+    // daca e scriere -> punem date pe d_in
+    if (trans.rd_wr == 0) begin
+      in_vif.driver_cb.d_in <= trans.d_in;
+    end
+
+    @(posedge in_vif.clk);
+    in_vif.driver_cb.valid_i <= 1'b0;
+
+    no_transactions++;
+  endtask
+
+  task main;
+    forever begin
+
+      while (in_vif.rst_ni == 0)
+        @(posedge in_vif.clk);
+
+      fork
+        begin
+          drive();
+        end
+
+        begin
+          reset();
+        end
+      join_any
+
+      disable fork;
+    end
+  endtask
+
+endclass
