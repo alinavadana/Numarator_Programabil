b7378c28eef7f4ecfb0913009efe8b3e261c8f72
alinavadana <123012409+alinavadana@users.noreply.github.com>
2026-04-29 13:56:36 +0300

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,9 +1,12 @@
-`include "intf_in.sv"
-`include "intf_out.sv"
+`include "interface_in.sv"
+`include "interface_out.sv"
+`include "counter_programabil.v"
 
-`include "random_test.sv"
+`include "test_simplu.sv"
+//`include "test.sv"
+//`include "random_test.sv"
 //`include "wr_rd_test.sv"
-`include "default_test.sv"
+//`include "default_test.sv"
 
 module testbench;
 
@@ -13,20 +16,20 @@ module testbench;
     always #5 clk = ~clk;
 
     initial begin
-        reset = 1;
-        #15 reset = 0;
+        reset = 0;
+        #15 reset = 1;
     end
 
-    intf_in in_intf(clk, reset);
-    intf_out out_intf(clk, reset);
+    interface_in in_intf(clk, reset);
+    interface_out out_intf(clk, reset);
 
-    default_test t1(in_intf, out_intf);
+    test_simplu t1(in_intf, out_intf);
 
     counter_programabil DUT (
         .clk_i(clk),
         .rst_ni(reset),
         .valid_i(in_intf.valid_i),
-        .rd_wr_i(in_intf.rd_wr_i),
+        .rd_wr_i(in_intf.rd_wr),
         .addr_i(in_intf.addr_i),
         .d_in(in_intf.d_in),
         .d_out(in_intf.d_out),
