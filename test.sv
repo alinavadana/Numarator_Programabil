8be0e069146f3c7b5db18e5adca08d1bdf9d0bd6
alinavadana <123012409+alinavadana@users.noreply.github.com>
2026-04-29 13:55:45 +0300

    Update test.sv

--- test.sv
+++ test.sv
@@ -6,11 +6,12 @@ program test(interface_in in_intf, interface_out out_intf);
     environment env;
 
     initial begin
+    $error("%0t a inceput testul", $time);
         // Instanțiem mediul trecând ambele interfețe către constructorul clasei environment 
         env = new(in_intf, out_intf);
         
         // Setăm numărul de tranzacții ce urmează a fi generate de generator_in 
-        env.gen.trans_cnt = 4;
+        env.gen.trans_cnt = 20;
         
         // Pornim execuția fluxului de test 
         env.run();
